93b44ee94e0a0025e9adf9ab5128c02e3453c70b
nikollllllas <nikollasohta2014@gmail.com>
2024-01-16 14:25:45 -0300

    :recycle:: updated media queries from Priorities and ShortTask

--- src/components/ShortTask/styles.js
+++ src/components/ShortTask/styles.js
@@ -1,20 +1,21 @@
 import styled from 'styled-components'
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints'
 
 export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   color: ${({ theme }) => theme.COLORS.GRAY_100};
-  height: 44px;
+  height: 3.75rem;
   border: 0;
-  padding: 0 16px;
-  border-radius: 7px;
-  font-size: 14px;
+  padding: 0 1rem;
+  border-radius: .5rem;
+  font-size: 0.87rem;
 
   display: flex;
   align-items: center;
-  gap: 7px;
+  gap: .5rem;
 
   > svg {
-    font-size: 18px;
+    font-size: 1.125rem;
     color: ${({ theme }) => theme.COLORS.RED_200};
   }
 `

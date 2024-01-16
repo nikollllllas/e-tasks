3cd45059afe397dee83129f3322f12d77b5060bb
nikollllllas <nikollasohta2014@gmail.com>
2024-01-16 14:51:56 -0300

    :sparkles:: created Burguer Button

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints'
 
 export const Container = styled.header`
   display: flex;
@@ -15,3 +16,18 @@ export const Container = styled.header`
     font-size: 2rem;
   }
 `
+
+export const Menu = styled.button`
+  background: none;
+  border: none;
+
+  > svg {
+    font-size: 2.5rem;
+    color: ${({ theme }) => theme.COLORS.BLUE_200};
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.MD}) {
+    display: none;
+  }
+`
+

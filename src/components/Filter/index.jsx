3008359a7d6f1a51117f414e0ab41c53171117d6
nikollllllas <nikollasohta2014@gmail.com>
2024-01-16 08:40:36 -0300

    :sparkles:: first commit

---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from './styles'
+
+export function Filter({ title, selected, ...rest }) {
+  return (
+    <Container
+      type='button'
+      selected={selected}
+      {...rest}>
+      {title}
+    </Container>
+  )
+}
